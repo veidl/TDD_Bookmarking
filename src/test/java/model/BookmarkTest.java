63f6b1f86a776cf09fd6f91d4c0527b98653c6d4
veidl <thomas.polzer@protonmail.com>
2022-05-13 20:32:46 +0200

    add url validator testcase

--- src/test/java/model/BookmarkTest.java
+++ src/test/java/model/BookmarkTest.java
@@ -1,4 +1,4 @@
-package bookmark;
+package model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
---
+++ src/test/java/model/UrlTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class UrlTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"https://google.at", "https://orf.at", "www.facebook.com"})
+    public void should_successfully_validate_url(String url) {
+        Assertions.assertTrue(new UrlValidator().validate(url));
+    }
+}
