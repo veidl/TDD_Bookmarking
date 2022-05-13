c9c72df31c17125f3aaf8eed23f72a378772195b
veidl <thomas.polzer@protonmail.com>
2022-05-13 20:44:50 +0200

    add url validation to bookmarks

--- src/main/java/model/Bookmark.java
+++ src/main/java/model/Bookmark.java
@@ -2,4 +2,16 @@ package model;
 
 public class Bookmark {
 
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        if (!UrlValidator.validate(url)) {
+            throw new IllegalArgumentException("Not a valid URL");
+        }
+        this.url = url;
+    }
 }
