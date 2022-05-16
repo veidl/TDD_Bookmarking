0dc9e38ddf6b470cb8e81cb53a0991cfcba69e61
veidl <thomas.polzer@protonmail.com>
2022-05-16 16:15:13 +0200

    create testcase for setting url in customurl class

--- src/test/java/at/ac/fhcampuswien/model/CustomUrlTest.java
+++ src/test/java/at/ac/fhcampuswien/model/CustomUrlTest.java
@@ -2,6 +2,8 @@ package at.ac.fhcampuswien.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CustomUrlTest {
 
@@ -10,5 +12,15 @@ public class CustomUrlTest {
     public void shouldCreateCustomUrl() {
         CustomUrl customUrl = new CustomUrl();
         Assertions.assertNotNull(customUrl);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"https://orf.at", "https://twitter.com", "https://www.google.at"})
+    public void shouldAddValidUrl(String url) {
+        CustomUrl customUrl = new CustomUrl();
+
+        customUrl.setUrl(url);
+
+        Assertions.assertEquals(url, customUrl.getUrl());
     }
 }
