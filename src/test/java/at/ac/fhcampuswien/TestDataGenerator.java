eb06a74b3b7f40356f7e485182d1fb079e499f6c
veidl <thomas.polzer@protonmail.com>
2022-05-16 17:14:53 +0200

    add bookmarkvalidator test

--- src/test/java/at/ac/fhcampuswien/TestDataGenerator.java
+++ src/test/java/at/ac/fhcampuswien/TestDataGenerator.java
@@ -15,4 +15,14 @@ public class TestDataGenerator {
 
         return bookmark;
     }
+
+    public static Bookmark getValidBookmarkWithoutTag() {
+        CustomUrl customUrl = new CustomUrl();
+        customUrl.setUrl("https://google.at");
+
+        Bookmark bookmark = new Bookmark();
+        bookmark.setCustomUrl(customUrl);
+
+        return bookmark;
+    }
 }
--- src/test/java/at/ac/fhcampuswien/util/BookmarkValidatorTest.java
+++ src/test/java/at/ac/fhcampuswien/util/BookmarkValidatorTest.java
@@ -1,5 +1,7 @@
 package at.ac.fhcampuswien.util;
 
+import at.ac.fhcampuswien.TestDataGenerator;
+import at.ac.fhcampuswien.model.Bookmark;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +10,16 @@ public class BookmarkValidatorTest {
     @Test
     public void shouldCreateBookmarkValidator() {
         Assertions.assertNotNull(new BookmarkValidator());
+    }
+
+    @Test
+    public void shouldSuccessfullyValidateBookmark() {
+        Bookmark validBookmarkWithTag = TestDataGenerator.getValidBookmarkWithTag();
+        Bookmark validBookmarkWithoutTag = TestDataGenerator.getValidBookmarkWithoutTag();
+
+        boolean actual = BookmarkValidator.validate(validBookmarkWithoutTag, validBookmarkWithTag);
+
+        Assertions.assertTrue(actual);
+
     }
 }
