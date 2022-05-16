344de1a16bf6d5ee3fbefd2531d75146b4796e78
veidl <thomas.polzer@protonmail.com>
2022-05-16 16:16:12 +0200

    add url to customurl class

--- src/test/java/at/ac/fhcampuswien/model/CustomUrl.java
+++ src/test/java/at/ac/fhcampuswien/model/CustomUrl.java
@@ -1,4 +1,14 @@
 package at.ac.fhcampuswien.model;
 
 public class CustomUrl {
+
+    String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
